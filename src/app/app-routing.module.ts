efb1ed7b16048b902fabbf9bbdf84ab9fee8c2f7
Sudhanthini <sudhanthini.a@oasys.co>
2022-03-24 15:58:46 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+import { AppComponent } from './app.component';
+import { MasterComponent } from './master/master.component';
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'list', component: MasterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
